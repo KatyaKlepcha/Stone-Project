17e6ac0f38f69dfeb2964403b4faf4d16e539dd9
KatyaKlepcha <solianchik@yandex.ru>
2024-05-20 19:01:28 +0300

    fix: add onIconClick

--- src/components/icon/Icon.tsx
+++ src/components/icon/Icon.tsx
@@ -8,11 +8,12 @@ export type IconProps = {
     viewBox?: string
     className?: string
     fill?: string
+    onIconClick?: () => void
 }
 
-export const Icon: FC<IconProps> = ({iconId, width, height, viewBox, className, fill}) => {
+export const Icon: FC<IconProps> = ({iconId, width, height, viewBox, className, fill, onIconClick}) => {
     return (
-        <svg width={width} height={height} viewBox={viewBox} className={className} fill={fill || "none"}
+        <svg width={width} height={height} viewBox={viewBox} className={className} fill={fill || "none"} onClick={onIconClick}
              xmlns="http://www.w3.org/2000/svg">
             <use xlinkHref={`${iconsSprite}#${iconId}`}></use>
         </svg>
